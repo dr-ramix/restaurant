6415f2186a9323357166f4b34faf4aeb53b5d169
dr-ramix <ramtin.hosseini1382@gmail.com>
2025-04-26 23:43:49 +0200

    Adding Template and Static files to the project

--- myrestaurant/urls.py
+++ myrestaurant/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from restaurant import urls as restaurant_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("restaurant/", include(restaurant_urls)),
 ]
