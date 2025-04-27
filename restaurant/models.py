c88769fc864fb186e647cf31677ede5d9467279b
dr-ramix <ramtin.hosseini1382@gmail.com>
2025-04-27 23:40:35 +0200

    Addint Category Model to models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255, db_index=True)
+    def __str__(self):
+        return self.title
