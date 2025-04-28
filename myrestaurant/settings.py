33289ade6f3da6eb35ac8dc3dde72a74408100c0
dr-ramix <ramtin.hosseini1382@gmail.com>
2025-04-28 14:11:06 +0200

    Adding gitignore and dotenv to project for hiding important secret files and values

--- myrestaurant/settings.py
+++ myrestaurant/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=uoms07s-na$eovf8b9#i6qiq_&y^cm-o%o1u^i=b&k%moai1t"
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +84,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "restaurant_db",
         "USER": "root",
-        "PASSWORD": "34993499",
+        "PASSWORD": os.getenv('DATABASE_PASSWORD'),
         "HOST": "localhost",
         "PORT": "3306",
         "OPTIONS": {
